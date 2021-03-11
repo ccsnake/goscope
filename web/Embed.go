82347668725ee2d1ab4cd36b0bc9cbfad840226c
J. J <josep@prowarehouse.nl>
2021-03-11 09:27:42 +0100

    Change to toml

--- web/Embed.go
+++ web/Embed.go
@@ -3,4 +3,4 @@ package web
 import "embed"
 
 //go:embed templates/goscope-components/*.gohtml templates/goscope-views/*.gohtml
-var TemplateFiles embed.FS
+var TemplateFiles embed.FS //nolint:gochecknoglobals
